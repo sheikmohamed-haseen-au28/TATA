0178f64204a3d6d40ba850d10ebe8bac96f0aebe
sheikmohamedhaseen-sheikakbarali-au27 <haszin93210@gmail.com>
2021-10-26 13:29:38 +0530

    and gate file added

---
+++ week2/arithmatic.py
@@ -0,0 +1,7 @@
+def AND(a, b):
+    return a & b
+print(" | AND Truth Table  Result |")
+print(" A = False, B = False  A AND B =",AND(False,False)," | ")
+print(" A = False, B = True  A AND B =",AND(False,True)," | ")
+print(" A = True, B = False  A AND B =",AND(True,False)," | ")
+print(" A = True, B = True  A AND B =",AND(True,True)," | ")
