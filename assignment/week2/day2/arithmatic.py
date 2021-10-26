175326467f7d072a12ee44c46ee40f1ab0a46049
sheikmohamedhaseen-sheikakbarali-au27 <haszin93210@gmail.com>
2021-10-26 22:46:47 +0530

    week2 day2 cc is added

--- assignment/week2/day2/arithmatic.py
+++
@@ -1,7 +0,0 @@
-def AND(a, b):
-    return a & b
-print(" | AND Truth Table  Result |")
-print(" A = False, B = False  A AND B =",AND(False,False)," | ")
-print(" A = False, B = True  A AND B =",AND(False,True)," | ")
-print(" A = True, B = False  A AND B =",AND(True,False)," | ")
-print(" A = True, B = True  A AND B =",AND(True,True)," | ")
---
+++ assignment/week2/day2/w2d2.py
@@ -0,0 +1,23 @@
+#q1
+a = 5.0
+b = 6
+c = a + b
+print(int(c))
+
+
+
+#q2
+p = 23.78
+s = 20
+l = "hello world"
+print (type(s))
+print(type(p))
+print(type(l))
+
+#q3
+x = int(input())
+y = int(input())
+z = print("addition" ,x + y)
+z = print("subtraction" ,x - y)
+z = print("multiply" ,x * y)
+z = print("division" ,x / y)
